772979e8c908b741e67c702619f67224b2d7ca72
Kyle Carter <kkcarter1129@gmail.com>
2020-01-22 18:15:54 -0600

    second commit

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./../styles/header.css"
+
+function Header() {
+    return (
+        <div>
+            <header className='header'>
+                <h1>Theme Park Guide</h1>
+                <ul className='navBar'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Parks</li>
+                    <li>LogIn/SignUp</li>
+                </ul>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
